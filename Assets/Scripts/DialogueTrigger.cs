d55b49d7e8529081d4778dd7bae36e39efbcb636
Evaline <evalinebai@gmail.com>
2024-03-10 10:37:09 -0700

    Add orb count

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [TextArea(3, 10)]
-    public string dialogueContent = "Hello, this is a sample dialogue. Press Enter to close.";
+    public string dialogueContent = "Orbs are $40 each to buy. You can sell them to me for $20 each.";
     private bool isPlayerNear = false;
 
     void OnTriggerEnter(Collider other)
